96720db812670dc16e735c1d424b2982bbaacfa8
Ian Stewart <90149902+IanStewart2001@users.noreply.github.com>
2023-03-21 21:56:05 -0400

    Added feature to add normal triad chords. A bug exists where after a certain chord interval, it doesn't loop back around the scale rwhich raises an IndexError.

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 #TODO:File currently outputs MIDI file that works in Reaper.
 #TODO:Write method that auto generates melody based on input key and various other parameters
 import random
+import roman
 from music21.chord import Chord
 from music21.duration import Duration
 from music21.instrument import Instrument
@@ -15,12 +16,13 @@ from music21.pitch import Pitch
 
 
 class Main:
-    def __init__(self, key, interval):
+    def __init__(self, key, interval, progression):
         #When defining key: uppercase means major, lowercase means minor...can also use 'M'/'m' respectively
         self.key = Key(key)
         self.scale = self.key.getPitches()
         self.s = Stream()
         self.interval = interval
+        self.progression = progression
         self.chord_stream = Stream()
     
     def Melody_in_Key(self, notes):
@@ -45,44 +47,51 @@ class Main:
         self.s.append(Note(self.key.getTonic()))
         self.fp = self.s.write('midi', fp='/Users/ian/Code/Python/Classes/Music/Melody.midi')
 
-
-    
-    def ChordProgression_in_Key(self):
-        '''
-        #Create random chord progression
-        #TODO:Iterate through each chord and remove notes that are 1 step apart to avoid clashing
-        self.chords = []
-        #Determines number of chords
-        for i in range(5):
-            chord = self.Create_Chord()
-            chord.duration = Duration(round(random.uniform(0.125, 4)/0.125) * 0.125)
-            self.chords.append(chord)
-            #print(chord.commonName)
-        for i in self.chords:
-            self.chord_stream.append(i)
-        self.fp = self.chord_stream.write('midi', fp='/Users/ian/Code/Python/Classes/Music/Chord_Progression.midi')
-        '''
-
-    def Create_Chord(self):
-        #Includes 4 notes in each chord. Add feature that randomly determines different number of notes
-        return Chord([random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())])
-    
     def Chord_Progression(self):
-        for i in self.scale:
-            print(i)
         #Proposed function for I IV V Progression
         self.chords = []
-        chord = Chord([self.scale[0], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
-        chord = Chord([self.scale[3], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
-        chord = Chord([self.scale[4], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
-        chord = Chord([self.scale[1], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
+        #If I want to ensure each chord is a seventh, add "self.scale[6]"
+        for interval in self.progression:
+            chord = Chord([self.scale[roman.roman_to_int(interval)-1], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
+        #TODO:Add feature to iterate through each chord and remove notes that are one semitone apart to avoid clashing
         for i in self.chords:
             self.chord_stream.append(i)
         self.fp = self.chord_stream.write('midi', fp='/Users/ian/Code/Python/Classes/Music/Chord_Progression.midi')
 
+    def Seventh_Progression(self):
+    #Proposed function for I IV V Progression
+        self.chords = []
+    #If I want to ensure each chord is a seventh, add "self.scale[6]"
+        #TODO:Below code through index error when it comes to the 4th, 5th, 6th chord etc. because it goes past the final item in the self.scale attribute. Find a way to loop back through the scale or iterate through the scale of the particular chord.
+        for interval in self.progression:
+            note = roman.roman_to_int(interval)
+            if str(interval).islower == True:
+                try:
+                    chord = Chord([self.scale[note-1], self.scale[note+2], self.scale[note+2]])
+                    self.chords.append(chord)
+                    print(chord)
+                except IndexError:
+                    continue
+            else:
+                try:
+                    chord = Chord([self.scale[note-1], self.scale[note+1], self.scale[note+3]])
+                    self.chords.append(chord)
+                    print(chord)
+                except IndexError:
+                    continue
+        for i in self.chords:
+            self.chord_stream.append(i)
+        self.fp = self.chord_stream.write('midi', fp='/Users/ian/Code/Python/Classes/Music/Chord_Progression.midi')
+        #chord = Chord([self.scale[roman.roman_to_int(interval)-1], random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches()), random.choice(self.key.getPitches())]); self.chords.append(chord)
+
+
+
+
+
+
 if __name__ == "__main__":
-    App = Main(key='Cm', interval=1)
+    App = Main(key='F', interval=1, progression=['I', 'IV', 'vi', 'V', 'I'])
     #App.Melody_in_Key(notes=random.randint(5, 5))
-    #App.ChordProgression_in_Key()
-    App.Chord_Progression()
+    #App.Chord_Progression()
+    App.Seventh_Progression()
 
